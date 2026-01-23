1a5b439619dce020884c50de4eb9ff958d308912
makkapakka0 <liyunfan199902@outlook.com>
2026-01-23 12:54:47 +0000

    Update Alpha_PCA.R

--- Alpha_PCA.R
+++ Alpha_PCA.R
@@ -6,6 +6,8 @@ library(ggfortify)
 library(sf)
 library(randomForest)
 library(fastshap)
+library(mgcv)
+library(gratia)
 
 data <- read.csv("D:\\tellsoilbio\\PCA\\Counts_withElements_Shannon.csv")
 
@@ -81,8 +83,8 @@ ggplot()+
                arrow=arrow(length=unit(0.3,'cm')),color='#d7483a')+
   ggrepel::geom_text_repel(data=env_df,aes(x=RDA1,y=RDA2,label=env),
                            color='#0f2b3a',size=3)+
-  xlim(-0.65,0.65)+
-  ylim(-0.65,0.65)+
+  xlim(-0.7,0.7)+
+  ylim(-0.7,0.7)+
   theme_minimal()+
   theme(axis.text = element_text(size=24),
         axis.title = element_text(size=28),
@@ -95,20 +97,99 @@ ggplot()+
 plot(rda_result,scaling=2)
 
 
+
+#RF
 all$lc<-data$LanduseSimplified
 rf<-randomForest(Collembola~lc+Sand+Silt+Clay+TC+TOC+TN+LOI+BD+Cu+Zn+Pb+Cd+Se+As+Ni+Fe+Al+Cr
                    +Hg+Mn+Sr+Mo+Co+Ba+U+Th+Ca+pH_CaCl2,
                  ntree=500,
                  mtry=15,
                  data=all)
+rf[["rsq"]]
+imp<-importance(rf)
+imp_df<-data.frame(feature=rownames(imp),importance=imp[,1])
+imp_df<-imp_df[order(imp_df$importance,decreasing = TRUE),]
+top_15<-head(imp_df,15)
+top<-rownames(top_15)
+
+rf_data_collem<-all[,colnames(all)%in%top]
+rf_data_collem<-cbind(all$Collembola,mod_data_collem)
+colnames(mod_data_collem)[colnames(mod_data_collem)=='all$Collembola']<-'Collembola'
+
+rf<-randomForest(Collembola~.,
+                 ntree=500,
+                 mtry=15,
+                 data=rf_data_collem)
+rf[["rsq"]]
 
 
+#GAM
+all$Silt[all$Silt<0]<-1
+all[,7:33]<-log(all[,7:33])
+
+#CV
+folds<-sample(rep(1:10,length.out=188))
+cv_error<-numeric(10)
+
+for (i in 1:10) {
+  train_data<-all[folds!=i,]
+  test_data<-all[folds==i,]
+  
+  gam<-gam(Collembola~factor(lc)+s(Sand)+s(Silt)+s(Clay)+s(TC)+s(TOC)+s(TN)+s(LOI)+s(BD)
+           +s(Cu)+s(Zn)+s(Pb)+s(Cd)+s(Se)+s(As)+s(Ni)+s(Fe)+s(Al)+s(Cr)
+           +s(Hg)+s(Mn)+s(Sr)+s(Mo)+s(Co)+s(Ba)+s(U)+s(Th)+s(Ca)+s(pH_CaCl2),
+           data=train_data,
+           select = FALSE,
+           method = 'REML')
+  preds<-predict(gam,newdata=test_data)
+  
+  cv_error[i]<-mean((test_data$Collembola-preds)^2)
+}
+
+error<-mean(cv_error)
 
 
+gam_collem<-gam(Collembola~factor(lc)+s(Sand)+s(Silt)+s(Clay)+s(TC)+s(TOC)+s(TN)+s(LOI)+s(BD)
+                +s(Cu)+s(Zn)+s(Pb)+s(Cd)+s(Se)+s(As)+s(Ni)+s(Fe)+s(Al)+s(Cr)
+                +s(Hg)+s(Mn)+s(Sr)+s(Mo)+s(Co)+s(Ba)+s(U)+s(Th)+s(Ca)+s(pH_CaCl2),
+                data=all,
+                select = FALSE,
+                method = 'REML'
+)
+summary(gam_collem)
+pred<-predict(gam_collem,newdata=all)
+mse<-mean((all$Collembola-pred)^2)
 
 
+gam_collem<-gam(Collembola~factor(lc)+s(Sand)+s(Silt)+s(Clay)+s(TC)+s(TOC)+s(TN)+s(LOI)+s(BD)
+                +s(Cu)+s(Zn)+s(Pb)+s(Cd)+s(Se)+s(As)+s(Ni)+s(Fe)+s(Al)+s(Cr)
+                +s(Hg)+s(Mn)+s(Sr)+s(Mo)+s(Co)+s(Ba)+s(U)+s(Th)+s(Ca)+s(pH_CaCl2),
+                data=all,
+                select = FALSE,
+                method = 'REML'
+                )
+summary(gam_collem)
 
+draw(gam_collem,select = 's(pH_CaCl2)')
+p_smooth<-smooth_estimates(gam_collem,smooth='s(Cd)')
+p_smooth
 
+ggplot()+
+  geom_point(data=all,aes(x=Cd,y=Collembola),alpha=0.5)+
+  geom_line(data=p_smooth,aes(x=Cd,y=.estimate),color='blue',size=1)
 
+gam_mites<-gam(Mites~factor(lc)+s(Sand)+s(Silt)+s(Clay)+s(TC)+s(TOC)+s(TN)+s(LOI)+s(BD)
+                +s(Cu)+s(Zn)+s(Pb)+s(Cd)+s(Se)+s(As)+s(Ni)+s(Fe)+s(Al)+s(Cr)
+                +s(Hg)+s(Mn)+s(Sr)+s(Mo)+s(Co)+s(Ba)+s(U)+s(Th)+s(Ca)+s(pH_CaCl2),
+                data=all,
+                select = FALSE,
+                method = 'REML')
+summary(gam_mites)
 
+draw(gam_mites,select = 's(Sr)')
+p_smooth<-smooth_estimates(gam_mites,smooth='s(LOI)')
+p_smooth
 
+ggplot()+
+  geom_point(data=all,aes(x=LOI,y=Mites),alpha=0.5)+
+  geom_line(data=p_smooth,aes(x=LOI,y=.estimate),color='blue',size=1)
